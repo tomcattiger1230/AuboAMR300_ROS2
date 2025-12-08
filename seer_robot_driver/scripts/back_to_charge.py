572c1c5fc6088a26692bb1f5377cb6c11e410d7e
Wei LUO <wei.luo.88@outlook.com>
2025-12-08 16:21:26 +0800

    UPdate

--- seer_robot_driver/scripts/back_to_charge.py
+++ seer_robot_driver/scripts/back_to_charge.py
@@ -4,19 +4,32 @@
 Author: Wei Luo
 Date: 2025-11-25 11:23:54
 LastEditors: Wei Luo
-LastEditTime: 2025-11-25 11:42:39
+LastEditTime: 2025-12-08 15:00:50
 Note: Note
 """
 
 import rclpy
+import os
 from rclpy.node import Node
+from std_srvs.srv import SetBool
 
 
 class BackToCharge(Node):
     def __init__(self):
-        super().__init__("back_to_charge_subscriber")
+        super().__init__("back_to_charge_server")
+        self.srv = self.create_service(SetBool, "charging", self.charging_callback)
+
+    def charging_callback(self, request, response):
+        if request.data is True:
+            # process the charging codes
+            response.success = True
+            self.get_logger().info("Calling back to charge service")
+
+        return response
 
 
 if __name__ == "__main__":
     rclpy.init()
     btc_node = BackToCharge()
+    rclpy.spin(btc_node)
+    rclpy.shutdown()
